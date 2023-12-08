dd294caf8721665f13932012d98090636901d440
Nuray <nuray.kavkaeva.04@gmail.com>
2023-12-09 02:47:22 +0600

    first commit

---
+++ src/main/java/com/example/hydroponics/HydroponicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hydroponics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroponicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydroponicsApplication.class, args);
+    }
+
+}
